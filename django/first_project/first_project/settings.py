17fb8d13051cd8efb9999123fb8598a84aafe9c2
dalin1907 <dalinlethi@gmail.com>
2026-01-05 17:32:01 +0700

    update

--- django/first_project/first_project/settings.py
+++ django/first_project/first_project/settings.py
@@ -17,7 +17,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 TEMPLATES_DIR = os.path.join(BASE_DIR, 'first_project', 'templates')
 STATIC_DIR = os.path.join(BASE_DIR, 'first_project', 'static')
 
-ALLOWED_HOSTS = [".vercel.app"]
+ALLOWED_HOSTS = ['127.0.0.1', 'localhost','.vercel.app']
 
 CSRF_TRUSTED_ORIGINS = [
     "https://*.ngrok-free.app",
