77d07610918d817f6d8c9096eddc506bc858693e
dalin1907 <dalinlethi@gmail.com>
2026-01-05 17:42:21 +0700

    update

--- django/first_project/first_project/settings.py
+++ django/first_project/first_project/settings.py
@@ -174,6 +174,7 @@ USE_TZ = True
 
 STATIC_URL = "static/"
 STATICFILES_DIRS = [STATIC_DIR,]
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 STATICFILES_FINDERS = [
     "django.contrib.staticfiles.finders.FileSystemFinder",
     "django.contrib.staticfiles.finders.AppDirectoriesFinder",
