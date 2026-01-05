bb48376b9f697b053439f89b587de5a2d7c32108
dalin1907 <dalinlethi@gmail.com>
2026-01-05 18:14:31 +0700

    update

--- django/first_project/first_project/settings.py
+++ django/first_project/first_project/settings.py
@@ -175,6 +175,8 @@ USE_TZ = True
 STATIC_URL = "static/"
 STATICFILES_DIRS = [STATIC_DIR,]
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 STATICFILES_FINDERS = [
     "django.contrib.staticfiles.finders.FileSystemFinder",
     "django.contrib.staticfiles.finders.AppDirectoriesFinder",
